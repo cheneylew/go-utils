cbbaa010fb2a8ab47762cbada4506827dc2a179f
CheneyLew <cheneylew@163.com>
2017-11-24 21:26:26 +0800

    update

--- utils/array.go
+++ utils/array.go
@@ -1,4 +1,4 @@
-package util
+package utils
 
 
 // Iterator is the function that accepts element of slice/array and its index
@@ -18,7 +18,7 @@ func Each(array []interface{}, iterator Iterator) {
 }
 
 // Map iterates over the slice and apply ResultIterator to every item. Returns new slice as a result.
-func Map(array []interface{}, iterator ResultIterator) []interface{} {
+func MapIt(array []interface{}, iterator ResultIterator) []interface{} {
 	var result = make([]interface{}, len(array))
 	for index, data := range array {
 		result[index] = iterator(data, index)
