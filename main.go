b575490bc19c67e1660f9754c55e6a0041b8daa3
CheneyLew <cheneylew@163.com>
2017-11-25 17:43:54 +0800

    add captcha

--- main.go
+++ main.go
@@ -1,13 +1,8 @@
 package main
 
 import (
-	"github.com/cheneylew/goutil/utils"
 )
 
 func main() {
-	utils.JJKPrintln("hello world")
-
-	p, n, v, e := utils.NewCaptcha("/Users/apple/Desktop",4,100,30)
-	utils.JJKPrintln(p, n, v, e)
-
+	HttpServerStart()
 }
