5595aca965aca012d44458c1cc9ef4430c4c8d48
Deju Liu <cheneylew@163.com>
2017-11-30 20:42:40 +0800

    add flow format

--- utils/bytes-string.go
+++ utils/bytes-string.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"unsafe"
+	"fmt"
 )
 
 // BytesToString convert []byte type to string type.
@@ -15,4 +16,18 @@ func StringToBytes(s string) []byte {
 	sp := *(*[2]uintptr)(unsafe.Pointer(&s))
 	bp := [3]uintptr{sp[0], sp[1], sp[1]}
 	return *(*[]byte)(unsafe.Pointer(&bp))
+}
+
+func FormatFlow(byteCount float64) string {
+	if byteCount < 1024 {
+		return fmt.Sprintf("%.2f byte", byteCount)
+	} else if byteCount < 1024*1024 {
+		return fmt.Sprintf("%.2f KB", byteCount/1024)
+	} else if byteCount < 1024*1024*1024 {
+		return fmt.Sprintf("%.2f MB", byteCount/(1024 * 1024))
+	} else if byteCount < 1024*1024*1024*1024 {
+		return fmt.Sprintf("%.2f GB", byteCount/(1024 * 1024*1024))
+	}
+
+	return ""
 }
