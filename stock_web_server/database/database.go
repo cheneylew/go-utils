5d3ffe30efcd3f399b8253338fa21c5cf13b2e56
Deju Liu <cheneylew@163.com>
2017-12-14 11:46:31 +0800

    update

--- stock_web_server/database/database.go
+++ stock_web_server/database/database.go
@@ -4,8 +4,6 @@ import (
 	"github.com/cheneylew/goutil/utils/beego"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/cheneylew/goutil/stock_web_server/models"
-
-	"github.com/cheneylew/goutil/utils/beego"
 )
 
 var DB DataBase
--- stock_web_server/main.go
+++ stock_web_server/main.go
@@ -3,9 +3,6 @@ package main
 import (
 	_ "github.com/cheneylew/goutil/stock_web_server/routers"
 	"github.com/astaxie/beego"
-	"github.com/astaxie/beego/orm"
-	"github.com/cheneylew/goutil/utils"
-	"github.com/cheneylew/goutil/stock_web_server/models"
 )
 
 func init() {
